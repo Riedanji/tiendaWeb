47ec2e0236cf5cfed8ac86b784b77426461937ee
riedanji <riedanji@gmail.com>
2025-03-27 11:22:53 -0500

    model users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150)
+    correo_electronico = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.nombre
+
+    USERNAME_FIELD = 'correo_electronico'
+    REQUIRED_FIELDS = ['nombre']
